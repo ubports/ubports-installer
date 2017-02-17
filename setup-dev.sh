e58acf09110f37d5d0a59c2f97f4aa8a3d51467c
Marius Gripsgard <mariogrip@ubuntu.com>
2017-02-17 14:27:26 +0100

    Update setup-dev.sh

--- setup-dev.sh
+++ setup-dev.sh
@@ -2,5 +2,5 @@
 
 sudo apt install gir1.2-glib-2.0 libglib2.0-dev libselinux-dev libssl-dev zlib1g-dev npm nodejs-legacy
 git submodule update --init --recursive
-make -C android-tool
+make -C android-tools
 npm install
