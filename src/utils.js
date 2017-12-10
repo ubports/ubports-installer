c3214eab37c5dd5167ce3a833939f7566d4d895d
neothethird <neo@neothethird.de>
2017-12-10 19:12:34 +0100

    Fixed escaping for weird chars in passwords. Wow. If this breaks again, i'm seriously going to reconsider my career choice.

--- src/utils.js
+++ src/utils.js
@@ -137,7 +137,7 @@ var checkPassword = (password, callback) => {
         return;
     }
     log.debug("checking password")
-    exec("echo '" + password.replace("'", "\\'") + "' | sudo -S echo correct", (err, output) => {
+    exec("echo \"" + password.replace(/\"/g, "\\\"") + "\" | sudo -S echo correct", (err, output) => {
         if(err){
             if (err.message.includes("incorrect password")) {
                 log.debug("incorrect password")
