88d0dec44ca623ca5ae8fff9842157fac1d3209f
neothethird <neo@neothethird.de>
2018-03-22 01:59:11 +0100

    Change indentation

--- src/system-image.js
+++ src/system-image.js
@@ -23,60 +23,60 @@ const ubuntuCommandFile = "ubuntu_command";
 const ubuntuPushDir = "/cache/recovery/"
 
 const getDeviceChannels = (device) => {
-    return systemImage.getDeviceChannels(device);
+  return systemImage.getDeviceChannels(device);
 }
 
 var downloadLatestVersion = (options) => {
-    console.log("downloadLatestVersion options: ", options);
-    var thisEvent;
-    if (!options.event)
-        thisEvent = new event();
-    else
-        thisEvent = options.event;
-    systemImage.getLatestVesion(options.device, options.channel).then((latest) => {
-        var urls = systemImage.getFilesUrlsArray(latest)
-        urls.push.apply(urls, systemImage.getGgpUrlsArray());
-        var files = systemImage.getFilePushArray(urls);
-        utils.downloadFiles(urls, thisEvent);
-        utils.log.debug(urls);
-        thisEvent.once("download:done", () => {
-            files.push({
-                src: systemImage.createInstallCommandsFile(systemImage.createInstallCommands(latest.files, options.installerCheck, options.wipe, options.enable), options.device),
-                dest: ubuntuPushDir + ubuntuCommandFile
-            });
-            thisEvent.emit("download:pushReady", files);
-        })
-    }).catch(() => {
-      thisEvent.emit("error", "could not find latest version; " + "device: " + options.device + " channel: " + options.channel);
+  console.log("downloadLatestVersion options: ", options);
+  var thisEvent;
+  if (!options.event)
+    thisEvent = new event();
+  else
+    thisEvent = options.event;
+  systemImage.getLatestVesion(options.device, options.channel).then((latest) => {
+    var urls = systemImage.getFilesUrlsArray(latest)
+    urls.push.apply(urls, systemImage.getGgpUrlsArray());
+    var files = systemImage.getFilePushArray(urls);
+    utils.downloadFiles(urls, thisEvent);
+    utils.log.debug(urls);
+    thisEvent.once("download:done", () => {
+      files.push({
+        src: systemImage.createInstallCommandsFile(systemImage.createInstallCommands(latest.files, options.installerCheck, options.wipe, options.enable), options.device),
+        dest: ubuntuPushDir + ubuntuCommandFile
+      });
+      thisEvent.emit("download:pushReady", files);
     });
-    return thisEvent;
+  }).catch(() => {
+    thisEvent.emit("error", "could not find latest version; " + "device: " + options.device + " channel: " + options.channel);
+  });
+  return thisEvent;
 }
 
 var pushLatestVersion = (files, thisEvent, dontWipeCache) => {
-    var doPush = () => {
-      adb.shell("mount -a", () => {
-        adb.shell("mkdir -p /cache/recovery", () => {
-          adb.pushMany(files, thisEvent);
-        })
-      })
-    }
-    if (dontWipeCache)
-      doPush()
-    else
-      adb.wipeCache(doPush);
-    return thisEvent;
+  var doPush = () => {
+    adb.shell("mount -a", () => {
+      adb.shell("mkdir -p /cache/recovery", () => {
+        adb.pushMany(files, thisEvent);
+      });
+    });
+  }
+  if (dontWipeCache)
+    doPush();
+  else
+    adb.wipeCache(doPush);
+  return thisEvent;
 }
 
 var installLatestVersion = (options) => {
-    var downloadEvent = downloadLatestVersion(options);
-    downloadEvent.once("download:pushReady", (files) => {
-        pushLatestVersion(files, downloadEvent)
-    });
-    return downloadEvent;
+  var downloadEvent = downloadLatestVersion(options);
+  downloadEvent.once("download:pushReady", (files) => {
+    pushLatestVersion(files, downloadEvent)
+  });
+  return downloadEvent;
 }
 
 module.exports = {
-    getDeviceChannels: getDeviceChannels,
-    downloadLatestVersion: downloadLatestVersion,
-    installLatestVersion: installLatestVersion
+  getDeviceChannels: getDeviceChannels,
+  downloadLatestVersion: downloadLatestVersion,
+  installLatestVersion: installLatestVersion
 }
