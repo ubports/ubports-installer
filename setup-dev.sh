dea697afc504ab652d7fececeb63d8cf93a017fa
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2018-11-17 15:04:14 +0100

    Use correct build dependencies for Ubuntu >= 18.04
    Fixes #521

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 distro=$(lsb_release -si)
+version=$(lsb_release -sr)
 
 if [ "$distro" = "openSUSE" ]; then
     packages="npm nodejs"
+elif [ "$distro" = "Ubuntu" -a ${version:0:2} -ge 18 ]; then
+    packages="npm nodejs libgconf2-4"
 else
     packages="npm nodejs-legacy"
 fi
