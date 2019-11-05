8c4d2557fbc340c9a37c931264f811cd7733f596
Jan Sprinz <neo@neothethird.de>
2019-11-05 19:32:44 +0100

    Change ui scripts to javascript

---
+++ src/html/scripts/dots.js
@@ -0,0 +1,7 @@
+const dots = window.setInterval( function() {
+    var wait = $("[id=wait-dot]");
+    if ( wait.text().length > 4 )
+        wait.text("");
+    else
+        wait.append(".");
+}, 400);
