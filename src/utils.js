04e981a9a0e8136a1867ba76a750d1b2d7d96c6f
Jan Sprinz <neo@neothethird.de>
2019-12-19 09:33:00 +0100

    Sanitize paths in spaces for the gazillionth time, fixes #847

--- src/utils.js
+++ src/utils.js
@@ -263,7 +263,9 @@ let toolpath = global.packageInfo.package
 let processes = [];
 function execTool(tool, args, callback) {
   let pid = exec(
-    [path.join(toolpath, tool)].concat(args).join(" "),
+    [
+      path.join(toolpath, tool).replace(" ", "\ ")
+    ].concat(args).join(" "),
     {
       maxBuffer: 1024 * 1024 * 2
     },
