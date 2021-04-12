2a4185953a0d7cfa243cd45bfd4478b09ec40ba7
Dalton Durst <dalton@ubports.com>
2021-04-12 17:29:18 -0500

    Catch and log early init errors
    Prior to this change, it was possible for errors initializing or waiting
    on plugins to go undetected. Errors in wait() would bubble up to the
    "no-network" error, which has been even more confusing for users.
    
    Related, but does not fix:
    https://github.com/ubports/ubports-installer/issues/1847

--- src/core/core.js
+++ src/core/core.js
@@ -66,40 +66,42 @@ class Core {
    * @returns {Promise}
    */
   prepare(file) {
-    return Promise.all([this.readConfigFile(file), this.plugins.init()]).then(
-      () => {
-        if (this.props.config) {
-          this.selectOs();
-        } else {
-          const wait = this.plugins
-            .wait()
-            .then(device => api.resolveAlias(device))
-            .catch(
-              e =>
-                new Promise(() => {
-                  log.debug(`failed to resolve device name: ${e}`);
-                  mainEvent.emit("user:no-network");
-                })
-            )
-            .then(device => {
-              if (device) {
-                log.info(`device detected: ${device}`);
-                this.setDevice(device);
-              }
-            });
-          ipcMain.once("device:selected", () => (wait ? wait.cancel() : null));
-          api
-            .getDeviceSelects()
-            .then(out => {
-              window.send("device:wait:device-selects-ready", out);
-            })
-            .catch(e => {
-              log.error("getDeviceSelects error: " + e);
-              window.send("user:no-network");
-            });
-        }
+    return Promise.all([
+      this.readConfigFile(file),
+      this.plugins.init().catch(e => errors.toUser(e, "initializing plugins"))
+    ]).then(() => {
+      if (this.props.config) {
+        this.selectOs();
+      } else {
+        const wait = this.plugins
+          .wait()
+          .catch(e => errors.toUser(e, "plugin wait()"))
+          .then(device => api.resolveAlias(device))
+          .catch(
+            e =>
+              new Promise(() => {
+                log.debug(`failed to resolve device name: ${e}`);
+                mainEvent.emit("user:no-network");
+              })
+          )
+          .then(device => {
+            if (device) {
+              log.info(`device detected: ${device}`);
+              this.setDevice(device);
+            }
+          });
+        ipcMain.once("device:selected", () => (wait ? wait.cancel() : null));
+        api
+          .getDeviceSelects()
+          .then(out => {
+            window.send("device:wait:device-selects-ready", out);
+          })
+          .catch(e => {
+            log.error("getDeviceSelects error: " + e);
+            window.send("user:no-network");
+          });
       }
-    );
+    });
   }
 
   /**
