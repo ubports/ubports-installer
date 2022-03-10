1f1543485f343e35a1293aa9e0b90b3155166254
Alexander Martinz <amartinz@shiftphones.com>
2022-03-10 15:27:31 +0100

    rollup: enable inlineDynamicImports
    This fixes:
      [!] Error: Invalid value "iife" for option "output.format" - UMD
      and IIFE output formats are not supported for code-splitting builds.
    
    See: https://rollupjs.org/guide/en/#outputinlinedynamicimports
    
    Change-Id: Ia4e793b924a878133ce7c279a65703bf043bc973
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -40,7 +40,8 @@ export default {
     sourcemap: true,
     format: "iife",
     name: "app",
-    file: "public/build/bundle.js"
+    file: "public/build/bundle.js",
+    inlineDynamicImports: true
   },
   plugins: [
     svelte({
