70e717a25f6a462c7895214387b0b81627b8472e
Alexander Martinz <amartinz@shiftphones.com>
2022-03-15 11:08:06 +0100

    settings: do not use schema for Store
    Using a schema causes avj to validate the schema passed into the
    store, but as we use non-standardized key names, it will fail
    the strict check.
    
    Pass in options instead of a schema, as these do not get strictly
    validated.
    
    This is in preparation to update to a new version of electron-store
    which updates avj and triggers this issue:
    
    -----
    
    A JavaScript error occurred in the main process
    Uncaught Exception:
    Error: strict mode: unknown keyword: "opencuts"
        at checkStrictMode (/android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/compile/util.js:174:15)
        at checkUnknownRules (/android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/compile/util.js:32:13)
        at alwaysValidSchema (/android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/compile/util.js:19:5)
        at /android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/vocabularies/applicator/properties.js:23:81
        at Array.filter (<anonymous>)
        at Object.code (/android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/vocabularies/applicator/properties.js:23:37)
        at keywordCode (/android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/compile/validate/index.js:454:13)
        at /android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/compile/validate/index.js:222:17
        at CodeGen.code (/android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/compile/codegen/index.js:439:13)
        at CodeGen.block (/android/halium/projects/ubports-installer/node_modules/conf/node_modules/ajv/dist/compile/codegen/index.js:568:18)
    
    -----
    
    Change-Id: Ib7ae997611697cc0c91d4da84e117f78e0f2a1c5
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*
- * Copyright (C) 2017-2020 UBports Foundation <info@ubports.com>
+ * Copyright (C) 2017-2022 UBports Foundation <info@ubports.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@ const Store = require("electron-store");
  * permanent UBports Installer settings
  */
 const settings = new Store({
-  schema: {
+  options: {
     animations: {
       type: "boolean",
       default: true
